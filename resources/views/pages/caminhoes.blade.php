32456db2585b7c6a8e5482f0c1ad071c213a8034
luan henrique <luan.henrique308@hotmail.com>
2023-11-16 05:17:19 -0300

    correcao modal caminhao

--- resources/views/pages/caminhoes.blade.php
+++ resources/views/pages/caminhoes.blade.php
@@ -15,7 +15,6 @@ $tabConfig = [
 @endphp
 
 <script src="{{ asset('js/operationAjax.js')}}"></script>
-<script src="{{ asset('utils/mask.js') }}"></script>
 <script src="{{ asset('utils/modals.js')}}"></script>
 <script src="{{ asset('vendor/open-admin/inputmask/inputmask.min.js') }}"></script>
 
